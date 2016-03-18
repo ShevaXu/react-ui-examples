0c0632c7dbbe3cec04a7c69fe86f64e6ac24d53b
Sheva Xu <shevaxu@outlook.com>
2016-03-18 14:01:54 +0800

    Clean up

--- src/index.jsx
+++ src/index.jsx
@@ -4,12 +4,11 @@ import './css/main.css'
 import React from 'react'
 import { render } from 'react-dom'
 import injectTapEventPlugin from 'react-tap-event-plugin'
-import HelloWorld from './components/hello-world'
 
 // Material-UI requires this before React V1.0 for onTouchTap
 injectTapEventPlugin()
 
 render(
-  <HelloWorld />,
+  <div>Hello World</div>,
   document.getElementById('root')
 )
