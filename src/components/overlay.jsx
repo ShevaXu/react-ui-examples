fa8696482a9fdf3a884009c8582b57adc3bd9c8b
Sheva Xu <shevaxu@outlook.com>
2016-03-18 14:00:09 +0800

    Update <Overlay />
    position from 'absolute' to 'fixed'
    add zIndex
    add props doc

--- src/components/overlay.jsx
+++ src/components/overlay.jsx
@@ -5,22 +5,35 @@ const defaultStyle = {
   width: '100vw',
   left: 0,
   top: 0,
-  position: 'absolute',
-  opacity: 0.8,
+  position: 'fixed',
   backgroundColor: '#000'
 }
 
+export const defaultProps = {
+  opacity: 0.8,
+  zIndex: 1
+}
+
 const Overlay = (props) => {
-  let style = props.opacity
-    ? Object.assign({}, defaultStyle, { opacity: props.opacity })
-    : defaultStyle
+  let style = Object.assign({}, defaultStyle,
+    { opacity: props.opacity || defaultProps.opacity, zIndex: props.zIndex || defaultProps.zIndex })
   return (
   props.onClick ? <div style={style} onClick={props.onClick} onTouchTap={props.onClick}></div> : <div style={style}></div>
   )
 }
 
 Overlay.propTypes = {
+  /**
+	 * The opacity of the overlay div, default 0.8
+	 */
   opacity: PropTypes.number,
+  /**
+   * The z-index of the overlay div, default 1
+   */
+  zIndex: PropTypes.number,
+  /**
+   * The callback function when click/touch-tap on the overlay div if provided
+   */
   onClick: PropTypes.func
 }
 
